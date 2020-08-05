d67b39d8b9cb0c10ce464a260e0e59dd30c1ff25
sinjo-sumito <s.sumitto@gmail.com>
2020-08-05 21:35:47 +0900

    items DB

---
+++ db/migrate/20200805075928_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.text :condition, null: false
+      t.integer :delivery_cost, null: false
+      t.string :sipping_area, null: false
+      t.string :sipping_days, null: false
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+end
