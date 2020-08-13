bd9882cbfeef7a67e2290c56470afebe3a2798c6
sinjo-sumito <s.sumitto@gmail.com>
2020-08-13 15:03:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "items#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users
+  
  
   resources :items, only: [:index] 
    
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     #   # get 'search_grandchild', defaults: { format: 'json' }
     # end
   # end  
-end
+
 
   resources :exhibition, only: :new
 
