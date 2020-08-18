7a165e3790af21e192a30755a0090e417a2f3b17
sinjo-sumito <s.sumitto@gmail.com>
2020-08-18 16:56:23 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
   def index
     @parents = Category.where(ancestry: nil)
-    # @parents = Category.all.order("ancestry ASC").limit(2)
   end
 end
