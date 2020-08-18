38d5ba432647b81999796e8bd352f78a9c2bc098
sinjo-sumito <s.sumitto@gmail.com>
2020-08-18 14:29:34 +0900

    Update catgories.js

--- app/assets/javascripts/catgories.js
+++ app/assets/javascripts/catgories.js
@@ -1,6 +1,5 @@
 $(function(){
   $('.leftTag1').hover(function() {
-    console.log(this);
     $(".category__parent--name").show();
     $(".category__child").hide();
     $(".category__grandchild").hide();
@@ -10,7 +9,6 @@ $(function(){
 });
 $(function(){
   $('.category__parent--name').hover(function() {
-    console.log(this);
     $(".category__child").hide();
     $(this).children().show();
     $(this).children().children().show();
@@ -20,12 +18,9 @@ $(function(){
 });
 $(function(){
   $('.category__child--name').hover(function() {
-    console.log(this);
     $(".category__grandchild").hide();
     $(this).children().show();
-    console.log(this);
     $(this).children().children().show();
-    
   }, function() {
     $(this).children().children().hide();
   });
