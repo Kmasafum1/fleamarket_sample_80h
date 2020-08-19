fb3bf70e10006c15ad72cdc8b11d26ae82e13b7b
takappe <green.na.ore.ha.bz@gmail.com>
2020-08-19 10:15:00 +0900

    カテゴリー実装後のルーティング修正

--- app/controllers/exhibition_controller.rb
+++ app/controllers/exhibition_controller.rb
@@ -1,16 +1,3 @@
 class ExhibitionController < ApplicationController
-  
-  def new
-    @category = Category.where(ancestry: nil)
-  end
-
-  def category_children
-    @category_chid = Category.find(params[:id]).children
-  end
-
-  def category_grandchildren
-    @category_grandchild = Category.find(params[:id]).children
-  end
-
 
 end
