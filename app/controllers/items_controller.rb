6425a505d63772c4abdb47df8463c3da5268b833
sinjo-sumito <s.sumitto@gmail.com>
2020-08-21 10:27:07 +0900

    削除機能実装終了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,12 +51,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
       
-      redirect_to root_path
-    else
-      render :edit
-    end
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
