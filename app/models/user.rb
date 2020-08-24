8370d24cbf45e7a75d36e32d203d177595ea6e56
Kmasafum1 <masafumi.kobayashi@glion.ch>
2020-08-24 11:37:34 +0900

    ユーザー単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
-  validates :nickname, presence: true
+  validates :nickname,:first_name,:last_name,:first_name_kana,:last_name_kana,:birthday, presence: true
   devise :validatable, password_length: 7..128
   has_one :address
   has_one :card, dependent: :destroy
